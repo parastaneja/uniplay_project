6eb6d2ce8e25fd2e7593ab86103c910274a78faa
thawanip <thawanip@uwindsor.ca>
2022-03-24 13:33:39 -0400

    Instantiated player cards

---
+++ Assets/Scripts/UnoBoardSpawnScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnoBoardSpawnScript : MonoBehaviour
+{
+    public GameObject[] cards;
+    public Transform[] point;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i<3; i++)
+        {
+            Instantiate(cards[i],point[i].transform.position, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
